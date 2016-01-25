635fce692fa52f5d8c248a37c2fb228d33e1d976
Zach La Celle <lacelle@roboticresearch.com>
2016-01-25 12:21:17 -0500

    Initial commit of the check_zfs_linux project.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# nagios_check_zfs_linux
-Nagios plugin to check ZFS pool in the Linux environment
+Check_ZFS_Linux
+===============
+
+A plugin to check ZFS pools in Linux
+------------------------------------
+
+Author: Zachary LaCelle
+License: GPLv3
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+### Description
+
+This is a python script which will check the health, capacity,
+fragmentation, and other things for use with Nagios monitoring.
+
+It provides perfdata feedback as well.
+
+### Currently tested OS/ZFS versions
+* Ubuntu 14.04 LTS, ZFS v5
---
+++ check_zfs.py
@@ -0,0 +1,311 @@
+#!/usr/bin/python
+
+########################################################################
+## 
+## Written by Zachary LaCelle
+## Copyright 2016
+## Licensed under GPL (see below) 
+##
+## Nagios script to monitor ZFS pools/filesystems
+## in Linux.
+##
+## Tested operating systems/ZFS versions:
+##  * Ubuntu 14.04.3 LTS, ZFS v5
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+########################################################################
+
+from sys import exit
+import subprocess
+import argparse
+from array import *
+from types import *
+
+##
+# Commands to run
+# CHANGE THESE IF YOU NEED TO
+##
+zpoolCommand='/sbin/zpool'
+zfsCommand='/sbin/zfs'
+
+##
+# Variables to print at the end
+##
+nagiosStatus=('OK','WARNING','CRITICAL','UNKNOWN')
+stateNum=0
+msg=''
+perfdata=''
+
+##
+# Filled from command line arguments
+##
+checkCapacity=False
+capWarnThreshold=0.5
+capCritThreshold=0.8
+checkFragmentation=False
+fragWarnThreshold=0.5
+fragCritThreshold=0.8
+
+def CheckArgFloatBounds( valueArr, minVal, maxVal ):
+    for value in valueArr:
+        if value < minVal:
+            return False
+        elif value > maxVal:
+            return False
+    return True
+
+def ConvertToGB( valueStr ):
+    value = valueStr[:-1]
+    if valueStr.endswith('G'):
+        return float(value)
+    elif valueStr.endswith('T'):
+        gigs=float(value)*1024
+        return float(gigs)
+    elif valueStr.endswith('M'):
+        gigs=float(value) / 1024.0
+        return float(gigs)
+    elif valueStr.endswith('K'):
+        gigs=float(value) / (1024.0 * 1024.0)
+        return float(gigs)
+
+def RaiseStateNum( stateNumIn, stateNum ):
+    if stateNumIn > stateNum:
+        stateNum=stateNumIn
+    return
+
+###################################################################################
+##
+# Parse command line args
+##
+parser = argparse.ArgumentParser(
+    prog='check_zfs',
+    description='Check the ZFS pool specified by an argument.',
+    epilog='Note that monitor flags (e.g. capacity) require 2 arguments: warning threshold, and critical threshold')
+parser.add_argument('--capacity', help="monitor utilization of zpool (%%, float [0-1]", type=float, nargs=2)
+parser.add_argument('--fragmentation', help="monitor fragmentation of zpool (%%, float [0-1])", type=float, nargs=2)
+parser.add_argument('pool', help="name of the zpool to check", type=str)
+
+args = parser.parse_args()
+
+retVal = True
+if args.capacity is not None:
+    checkCapacity=True
+    capWarnThreshold=args.capacity[0]
+    capCritThreshold=args.capacity[1]
+    capArr = array('d', [capWarnThreshold, capCritThreshold])
+    retVal = CheckArgFloatBounds(capArr, 0.0, 1.0)
+    if retVal is False:
+        RaiseStateNum(3, stateNum)
+        print nagiosStatus[stateNum] + ": Capacity thresholds must be between 0 and 1."
+        parser.print_help()
+        exit(stateNum)
+retVal = True
+if args.fragmentation is not None:
+    checkFragmentation=True
+    fragWarnThreshold=args.fragmentation[0]
+    fragCritThreshold=args.fragmentation[1]
+    fragArr = array('d', [fragWarnThreshold, fragCritThreshold])
+    retVal = CheckArgFloatBounds(fragArr, 0.0, 1.0)
+    if retVal is False:
+        RaiseStateNum(3, stateNum)
+        print nagiosStatus[stateNum] + ": Fragmentation thresholds must be between 0 and 1."
+        parser.print_help()
+        exit(stateNum)
+###################################################################################
+
+###################################################################################
+##
+# Get generic info about the ZFS environment
+zfsEntries = []
+zfsArgs=' list'
+fullCommand='sudo -n ' + zfsCommand + zfsArgs
+childProcess = subprocess.Popen(fullCommand, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+zfsString = childProcess.communicate()[0]
+zfsRetval = childProcess.returncode
+
+if zfsRetval is 1:
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": process must be run as root. Possible solution: add the following to your visudo: nagios ALL=NOPASSWD: /sbin/zfs"    
+    exit(stateNum)
+
+zfsLines = zfsString.splitlines()
+for idx, line in enumerate(zfsLines):
+    if idx != 0:
+        zfsEntry=line.split()
+        zfsEntries.append(zfsEntry)
+
+# Make sure the pool we specified is valid
+validPool=False
+for entry in zfsEntries:
+    if entry[0] == args.pool:
+        validPool=True
+if not validPool:
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": Pool " + args.pool + " is invalid. Please select a valid pool."
+    exit(stateNum)
+
+###################################################################################
+##
+# Get info on zpool
+
+zpoolArgs=' list ' + args.pool;
+fullCommand='sudo -n ' + zpoolCommand + zpoolArgs
+
+childProcess = subprocess.Popen(fullCommand, shell=True, stdin=subprocess.PIPE, 
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+zpoolString = childProcess.communicate()[0]
+zpoolRetval = childProcess.returncode
+
+if zpoolRetval is 1:
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": process must be run as root. Possible solution: add the following to your visudo: nagios ALL=NOPASSWD: /sbin/zpool"
+    exit(stateNum)
+
+zpoolLines=zpoolString.splitlines()
+zpoolMeta=zpoolLines[0].split()
+zpoolMetaStr=','.join(zpoolMeta)
+zpoolEntry=zpoolLines[1].split()
+zpoolEntryStr=','.join(zpoolEntry)
+
+name=''
+size=''
+alloc=''
+free=''
+expandsz=''
+frag=''
+cap=''
+dedup=''
+health=''
+altroot=''
+
+for idx, fieldName in enumerate(zpoolMeta):
+    if fieldName=='NAME':
+        name=zpoolEntry[idx]
+    elif fieldName=='SIZE':
+        size=zpoolEntry[idx]
+    elif fieldName=='ALLOC':
+        alloc=zpoolEntry[idx]
+    elif fieldName=='FREE':
+        free=zpoolEntry[idx]
+    elif fieldName=='EXPANDSZ':
+        expandsz=zpoolEntry[idx]
+    elif fieldName=='FRAG':
+        frag=zpoolEntry[idx]
+    elif fieldName=='CAP':
+        cap=zpoolEntry[idx]
+    elif fieldName=='DEDUP':
+        dedup=zpoolEntry[idx]
+    elif fieldName=='HEALTH':
+        health=zpoolEntry[idx]
+    elif fieldName=='ALTROOT':
+        altroot=zpoolEntry[idx]
+
+if name=='':
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": Missing required field in zpool output: NAME"
+    exit(stateNum)
+if health=='':
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": Missing required field in zpool output: HEALTH"
+    exit(stateNum)
+if checkCapacity and cap=='':
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": Cannot monitor capacity without zpool output: CAP. Outputs are" + zpoolMetaStr
+    exit(stateNum)
+if checkFragmentation and frag=='':
+    RaiseStateNum(3, stateNum)
+    print nagiosStatus[stateNum] + ": Cannot monitor fragmentation without zpool output: FRAG. Outputs are " + zpoolMetaStr
+    exit(stateNum)
+
+###################################################################################
+##
+# OK, finally in the actual status checking of the zpool
+
+# Let's build up our perfdata, regardless of what we're checking
+if frag!='':
+    fragPercent=frag.replace("%", "")
+    fragFraction=float(fragPercent) / 100.0
+    fragPerfStr="frag="+str(fragFraction)+";"
+    if checkFragmentation:
+        fragPerfStr=fragPerfStr+str(fragWarnThreshold)+";"+str(fragCritThreshold)+";"
+    else:
+        fragPerfStr+=(";;");
+    perfdata+=(fragPerfStr)
+    perfdata+=" "
+
+if cap!='':
+    capPercent=cap.replace("%", "")
+    capFraction=float(capPercent) / 100.0
+    capPerfStr="cap="+str(capFraction)+";"
+    if checkCapacity:
+        capPerfStr=capPerfStr+str(capWarnThreshold)+";"+str(capCritThreshold)+";"
+    else:
+        capPerfStr+=(";;");
+    perfdata+=(capPerfStr)
+    perfdata+=" "
+    
+# Sizes can be in K, M, G, or T (maybe P, but I'm not doing this yet)
+if size!='':
+    sizeGB = ConvertToGB(size)
+    perfdata+="size="+str(sizeGB)+";;;"
+    perfdata+=" "
+
+if alloc!='':
+    allocGB = ConvertToGB(alloc)
+    perfdata+="alloc="+str(allocGB)+";;;"
+    perfdata+=" "
+
+if free!='':
+    freeGB = ConvertToGB(free)
+    perfdata+="free="+str(freeGB)+";;;"
+    perfdata+=" "
+
+healthNum=-1
+if health=='ONLINE':
+    healthNum=0
+elif health=='OFFLINE':
+    RaiseStateNum(1, stateNum)
+    healthNum=1
+elif health=='REMOVED':
+    RaiseStateNum(1, stateNum)
+    healthNum=2
+elif health=='UNAVAIL':
+    RaiseStateNum(1, stateNum)
+    healthNum=3
+elif health=='DEGRADED':
+    RaiseStateNum(2, stateNum)
+    healthNum=4
+elif health=='FAULTED':
+    RaiseStateNum(2, stateNum)
+    healthNum=5
+perfdata+="health="+str(healthNum)+";1;3;"
+perfdata+=" "
+
+msg="POOL: "+str(name)+", STATUS: "+health
+if size!='':
+    msg+=", SIZE: "+str(size)
+if alloc!='':
+    msg+=", ALLOC: "+str(alloc)
+if free!='':
+    msg+=", FREE: "+str(free)
+if frag!='':
+    msg+=", FRAG: "+str(frag)
+if cap!='':
+    msg+=", CAP: "+str(cap)
+
+##
+# Print our output and return
+print nagiosStatus[stateNum]+": "+msg+" | "+perfdata
+exit(stateNum)
